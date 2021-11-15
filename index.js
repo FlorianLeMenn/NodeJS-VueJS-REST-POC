69e84dd41e3b9be86b0c3a9fa7a81163d48bbab0
FlorianLeMenn <florian.le.menn@gmail.com>
2021-11-15 19:00:51 +0100

    ajout body parser & middleware custom

--- index.js
+++ index.js
@@ -7,12 +7,23 @@ const app       = express();
 const router    = require('./app/router');
 const PORT      = process.env.PORT || 3000;
 
-//Gestion des session
+//Gestion du body parser: rendre clair les réponses des forms (post)
+app.use(express.urlencoded({extended:true}));
+//app.use(express.json());
+
+//Gestion des sessions
 app.use(session({
     saveUninitialized: true,
     resave: true,
-    secret: 'secret phrase'
+    secret: process.env.SECRET_SESSION
 }));
+
+//Gestion des variables à attacher aux views pour l'utiliser partout, middleware custom
+app.use((req, res, next) => {
+    if(req.session.login) res.locals.login = req.session.login
+    //on passe au prochaine middleware, sinon on reste bloqué dans celui-ci
+    next();
+});
 
 // On dis à notre application express d'utiliser le router que l'on a récupérer de notre fichier router.js
 app.use(router);
